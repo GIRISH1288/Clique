c84f748a80c19c70ecce19e6c479b7b825f7f7ca
udayshinde75 <dadashinde247@gmail.com>
2024-03-05 12:02:06 +0530

    Log in Activity Created

--- app/src/main/java/com/project/myapplication/SignIn.java
+++ app/src/main/java/com/project/myapplication/SignIn.java
@@ -3,13 +3,23 @@ package com.project.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class SignIn extends AppCompatActivity {
-
+    EditText etSignInEmail, etSignInPassword;
+    Button btnLogIn;
+    LottieAnimationView animLoadingSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        etSignInEmail = findViewById(R.id.etSignInEmail);
+        etSignInPassword = findViewById(R.id.etSignInPassword);
+        btnLogIn = findViewById(R.id.btnLogIn);
+
 
         overridePendingTransition(R.anim.activity_enter, R.anim.activity_exit);
     }
